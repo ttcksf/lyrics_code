72156df68af060d07e9d2742c2fca3f685c84240
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-12-02 05:10:52 +0900

    ongoing songcard

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        'a8b4041c9amsh767f81ab6a76693p1cd8f5jsn104b9b213d88'
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+export const { useGetTopChartsQuery } = shazamCoreApi;
